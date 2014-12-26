370caefbb74f2532db417356ccba1818c51ac33b
long5281 <long_5281@163.com>
2014-12-26 16:46:19 +0800

    second

---
+++ platforms/android/assets/www/plugins/com.cmpsoft.mobile.plugin.pushnotification/www/PushNotification.js
@@ -0,0 +1,35 @@
+cordova.define("com.cmpsoft.mobile.plugin.pushnotification.FGPushNotification", function(require, exports, module) { var cordova = require('cordova'),
+    exec = require('cordova/exec');
+
+var FGPushNotification = function() {
+	this.registered = false;
+
+};
+
+
+
+FGPushNotification.prototype.init = function(api_key)
+{
+    exec(fastgoPushNotification.successFn, fastgoPushNotification.failureFn, 'FGPushNotification', 'init', [api_key]);
+};
+
+FGPushNotification.prototype.successFn = function(info)
+{
+    alert("channelID:"+info.channel_id+"\t userID："+info.user_id);
+	if(info){
+		fastgoPushNotification.registered = true;
+		cordova.fireDocumentEvent("cloudPushRegistered", info);		
+	}
+};
+
+FGPushNotification.prototype.failureFn = function(info)
+{
+	fastgoPushNotification.registered = false;
+};
+
+
+
+var fastgoPushNotification = new FGPushNotification();
+
+module.exports = fastgoPushNotification;
+});
